d4a239437caa31437dcd3c738d6ec82b4ab2d323
XhmikosR <xhmikosr@gmail.com>
2026-05-12 19:24:52 +0300

    Update dependencies and drop Node.js < 20 support

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const assert = require('assert').strict;
+const assert = require('node:assert').strict;
 const { test } = require('uvu');
 const detective = require('../index.js');
 
