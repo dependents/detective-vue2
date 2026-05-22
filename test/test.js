61f127211a8daf247e530ffa3906bf3f4094a7d8
XhmikosR <xhmikosr@gmail.com>
2026-05-22 15:30:55 +0300

    tests: streamline fixtures (#30)
    * tests: streamline fixtures
    
    * Add stylus test
    
    * Increase coverage

--- test/test.js
+++ test/test.js
@@ -15,60 +15,137 @@ test('throws if the source content is not a string', () => {
 });
 
 test('retrieves the dependencies of script block with lang ts', () => {
-  const deps = detective('<template></template> <script lang="ts">import {foo, bar} from "mylib"; </script>');
+  const fixture = `<template></template>
+<script lang="ts">
+import { foo, bar } from "mylib";
+</script>
+`;
+  const deps = detective(fixture);
   assert.equal(deps.length, 1);
   assert.equal(deps[0], 'mylib');
 });
 
 test('retrieves the dependencies of scss and script block with lang ts', () => {
-  const deps = detective('<template></template> <script lang="ts">import {foo, bar} from "mylib"; </script> <style lang="scss">@import \'vars.scss\' </style>');
+  const fixture = `<template></template>
+<script lang="ts">
+import { foo, bar } from "mylib";
+</script>
+<style lang="scss">
+@import 'vars.scss';
+</style>
+`;
+  const deps = detective(fixture);
   assert.equal(deps.length, 2);
   assert.equal(deps[0], 'mylib');
   assert.equal(deps[1], 'vars.scss');
 });
 
 test('retrieves the dependencies of scss and script block with lang js', () => {
-  const deps = detective('<template></template> <script>import {foo, bar} from "mylib"; </script> <style lang="sass">@import \'vars.scss\' </style>');
+  const fixture = `<template></template>
+<script>
+import { foo, bar } from "mylib";
+</script>
+<style lang="sass">
+@import 'vars.scss'
+</style>
+`;
+  const deps = detective(fixture);
   assert.equal(deps.length, 2);
   assert.equal(deps[0], 'mylib');
   assert.equal(deps[1], 'vars.scss');
 });
 
 test('retrieves the dependencies of less and script block with lang js', () => {
-  const deps = detective('<template></template> <script>import {foo, bar} from "mylib"; </script> <style lang="less">@import \'vars.scss\' </style>');
+  const fixture = `<template></template>
+<script>
+import { foo, bar } from "mylib";
+</script>
+<style lang="less">
+@import 'vars.less';
+</style>
+`;
+  const deps = detective(fixture);
   assert.equal(deps.length, 2);
   assert.equal(deps[0], 'mylib');
-  assert.equal(deps[1], 'vars.scss');
+  assert.equal(deps[1], 'vars.less');
+});
+
+test('retrieves the dependencies of stylus and script block with lang js', () => {
+  const fixture = `<template></template>
+<script>
+import { foo, bar } from "mylib";
+</script>
+<style lang="stylus">
+@import 'vars.styl';
+</style>
+`;
+  const deps = detective(fixture);
+  assert.equal(deps.length, 2);
+  assert.equal(deps[0], 'mylib');
+  assert.equal(deps[1], 'vars.styl');
 });
 
 test('retrieves the dependencies of script block with lang ts using setup syntax', () => {
-  const deps = detective(`<script lang="ts" setup>
+  const fixture = `<script lang="ts" setup>
 import { foo, bar } from "mylib";
 import OtherComponent from "./OtherComponent.vue";
 </script>
 <template>
-<OtherComponent />
+  <OtherComponent />
 </template>
-`);
+`;
+  const deps = detective(fixture);
   assert.equal(deps.length, 2);
   assert.equal(deps[0], 'mylib');
   assert.equal(deps[1], './OtherComponent.vue');
 });
 
 test('retrieves the dependencies of script block with lang ts using both setup and normal syntax', () => {
-  const deps = detective(`<script lang="ts">
+  const fixture = `<script lang="ts">
 import { foo, bar } from "mylib";
 </script>
 <script setup lang="ts">
 import OtherComponent from "./OtherComponent.vue";
 </script>
 <template>
-<OtherComponent />
+  <OtherComponent />
 </template>
-`);
+`;
+  const deps = detective(fixture);
   assert.equal(deps.length, 2);
   assert.equal(deps[0], 'mylib');
   assert.equal(deps[1], './OtherComponent.vue');
+});
+
+test('retrieves the dependencies of script setup block without lang', () => {
+  const fixture = `<script setup>
+import { foo, bar } from "mylib";
+import OtherComponent from "./OtherComponent.vue";
+</script>
+<template>
+  <OtherComponent />
+</template>
+`;
+  const deps = detective(fixture);
+  assert.equal(deps.length, 2);
+  assert.equal(deps[0], 'mylib');
+  assert.equal(deps[1], './OtherComponent.vue');
+});
+
+test('ignores plain CSS style blocks', () => {
+  const fixture = `<template></template>
+<script>
+import { foo, bar } from "mylib";
+</script>
+<style>
+.foo {
+  color: red;
+}
+</style>
+`;
+  const deps = detective(fixture);
+  assert.equal(deps.length, 1);
+  assert.equal(deps[0], 'mylib');
 });
 
 test.run();
